9162c111133d43c3f176cfcb25f35c9cf842fc56
BekzhanJoy <161342488+BekzhanJoy@users.noreply.github.com>
2024-02-26 21:38:47 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+// Ba Du Ao Tian October 10, 2019 https://github.com/AJLoveChina/birthday
+var config = {
+    // The length of each sentence can be arbitrary, you can write ten or twenty sentences
+    // Try to keep each sentence within 15 characters for better display
+    texts: [
+        "To my beloved",      // Remember, the last comma of each sentence here must be in English!! Very important!!
+        "Cutie pie",  
+        "Today is your birthday",
+        "It's our",
+        "Third birthday together",
+        "Last year's birthday",
+        "I remember we ate at Wang Po skewers",
+        "This year, we'll eat well",
+        "Gonna feed my cute piggy",
+        "Then take you away",
+        "YAMI~~",
+    ],
+    imgs: {
+        "Cutie pie": "./imgs/xiaokeai.png",
+    },
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "Seems like something is missing",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
